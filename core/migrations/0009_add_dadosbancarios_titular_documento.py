e4cb742c8f194b67036cbd05149d31a08368f218
mininim666 <andreribeirobrt@gmail.com>
2025-10-02 16:17:13 -0300

    chore(core): ajuste final migração 0009 (titular/documento_titular)

--- core/migrations/0009_add_dadosbancarios_titular_documento.py
+++ core/migrations/0009_add_dadosbancarios_titular_documento.py
@@ -11,17 +11,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="dadosbancarios",
             name="titular",
-            field=models.CharField(
-                default="TITULAR", max_length=100, verbose_name="Titular da Conta"
-            ),
+            field=models.CharField(default="TITULAR", max_length=100, verbose_name="Titular da Conta"),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name="dadosbancarios",
             name="documento_titular",
-            field=models.CharField(
-                default="00000000000", max_length=20, verbose_name="CPF/CNPJ do Titular"
-            ),
+            field=models.CharField(default="00000000000", max_length=20, verbose_name="CPF/CNPJ do Titular"),
             preserve_default=False,
         ),
     ]
