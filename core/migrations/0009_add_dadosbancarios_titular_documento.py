6a31ed115891efc0031972d5a3b16bd1a6f36105
mininim666 <andreribeirobrt@gmail.com>
2025-10-02 16:12:50 -0300

    feat(core): add titular e documento_titular em DadosBancarios (migração 0009)

---
+++ core/migrations/0009_add_dadosbancarios_titular_documento.py
@@ -0,0 +1,27 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0008_role_is_active"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="dadosbancarios",
+            name="titular",
+            field=models.CharField(
+                default="TITULAR", max_length=100, verbose_name="Titular da Conta"
+            ),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name="dadosbancarios",
+            name="documento_titular",
+            field=models.CharField(
+                default="00000000000", max_length=20, verbose_name="CPF/CNPJ do Titular"
+            ),
+            preserve_default=False,
+        ),
+    ]
